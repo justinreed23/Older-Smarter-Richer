e73e6956d724c69e635e0f6a266872469267d0ca
JustinReed <jtr223@lehigh.edu>
2024-04-29 14:45:08 -0400

    Worked on subpages

---
+++ pages/Meet_The_Team.py
@@ -0,0 +1,18 @@
+"""
+## About Us:
+### Reghan Hesser
+title: "Reghan Hesser" 
+logo: "/images/headshot.jpg"
+- Major: Finance 
+- Year of Graduation: 2024
+- Interests: Cooking, Skiing, Dancing 
+### Justin Reed
+- Major:
+- Year of Graduation: 2024
+- Interests: 
+### Maria Maragkelli
+- Major:
+- Year of Graduation: 2024
+- Interests: 
+
+"""
